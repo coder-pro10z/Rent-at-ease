70418f2ed0c141d58e8cad73da47c85e8fa32194
Praveen <2pkashyap2001@gmail.com>
2023-02-08 04:03:03 +0530

    Search date button styles to home page cards to add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './Home.js'
 import Header from './Header.js'
 import Footer from './Footer';
 import Login from './Login';
+import Welcome from './Welcome';
+
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 // import SearchPage from './SearchPage';
 // import Navbar from './Navbar';
@@ -22,6 +24,8 @@ function App() {
           </Route>  */}
            <Route path='/' element={<Home/>} />
            <Route path='/login' element={<Login/>} />
+           <Route path='/welcome' element={<Welcome/>} />
+           
           {/* searchPage */}
          {/* <Route path='/search'>
            <SearchPage/>
---
+++ src/Search.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+import './Search.css'
+import "react-date-range/dist/styles.css"
+//main style file
+import "react-date-range/dist/theme/default.css"
+//theme style file
+import {DateRangePicker} from "react-date-range";
+import { Button } from '@material-ui/core';
+
+// import { PeopleIcon } from '@material-ui/icons/People';
+import PeopleIcon from '@material-ui/icons/People';
+// import {useHistory} from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+// navigate('/home');
+// DATE PICKER COMPONENT
+function Search() {
+    const navigate = useNavigate();
+    // const history=useHistory();
+const [startDate,setStartDate]= useState(new Date());
+const [endDate,setEndDate]= useState(new Date());
+
+const selectionRange={
+    startDate:startDate,
+    endDate:endDate,
+    key:"selection",};
+    function handleSelect(ranges){
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+        }
+return (
+    <div className='search'>
+        <DateRangePicker ranges={[selectionRange]}
+        onChange={handleSelect}/>
+        
+        <h2>
+            Number of guests
+            <PeopleIcon/>
+        </h2>
+        <input min={0} defaultValue={2} type="number" />
+        <Button onClick={()=>navigate('/search')} >Search Airbnb</Button>
+        
+    </div>
+  )
+}
+
+export default Search
